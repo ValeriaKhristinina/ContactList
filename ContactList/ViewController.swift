1be615d6ceed28730aec8cd56e3e0dcd380377d3
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2020-02-19 12:29:24 +0100

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Valeria Khristinina on 19/02/2020.
+//  Copyright © 2020 Valeria Khristinina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
